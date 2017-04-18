dcd140c744655c37a50c161dfeaa031fae7d0064
Jonas <jkpaskus@gmail.com>
2017-04-17 22:55:38 -0400

    First commit for validation methods

---
+++ Validation Bonus Lab/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation_Bonus_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number boss: ");
+            string prompt = Console.ReadLine();
+            int value = GetInt(prompt);
+            Console.WriteLine("Here is the value: {0}", value);
+           
+        }
+
+        public static int GetInt(string prompt)
+        {
+            int value;
+            if(!int.TryParse(prompt, out value))
+            {
+                Console.WriteLine("Please enter a proper NUMERIC integer!!");
+
+            } return value;
+            
+        }
+    }
+}
